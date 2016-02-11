49d13f7fcfad6dc0fc96246612e7bdf713ad5d47
Colin Diener <colin@colind.me>
2016-02-11 09:53:18 -0800

    added some more commands

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import telepot
 import time
 from commands import *
 from decimal import *
+from users import *
 
 # Get the API key
 parser = SafeConfigParser()
@@ -13,12 +14,17 @@ api = parser.get('shadsbot_settings', 'api_key')
 def handle(msg):
 	chat_id = msg['chat']['id']
 	command = msg['text']
-        checkcmd(command,chat_id,bot)
+        checkcmd(command,chat_id,bot,colin,julian,jake)
         
 bot = telepot.Bot(api)
 bot.setWebhook()
 bot.notifyOnMessage(handle)
 getcontext().prec = 3
+#create users - this is not permanent
+colin = User("Colin", 0)
+julian = User("Julian", 50)
+jake = User("Jake", 100)
+
 print('ShadsBot has started. Ready when you are.')
 
 while 1:
